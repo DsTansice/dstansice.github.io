188e1cf9d112d7c93e4be3e0c38f352882cd62e2
[GithubUsername] <[GithubEmail]>
2021-03-31 03:55:17 +0000

    Site updated: 2021-03-31 03:55:16

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,17 +1,1 @@
-var OriginTitle = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/joke.ico");
-         document.title = '看不见我🙈~看不见我🙈~';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = ' ( ๑•̀ㅂ•́) ✧竟然被你发现了～';
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
- });
-
+"use strict";var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/joke.ico"),document.title="看不见我🙈~看不见我🙈~",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/favicon.ico"),document.title=" ( ๑•̀ㅂ•́) ✧竟然被你发现了～",titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
